42f454127e3456d7f045455049e034ea7b5d1f2f
Treinamento HBSIS <treinamento@hbsis.com.br>
2019-08-07 11:59:10 -0300

    calc

--- WpfApp1/Views/usCalc.xaml.cs
+++ WpfApp1/Views/usCalc.xaml.cs
@@ -20,9 +20,160 @@ namespace WpfApp1.Views
     /// </summary>
     public partial class usCalc : UserControl
     {
+        double acumula = 0;
+        string operacao = "";
         public usCalc()
         {
             InitializeComponent();
+        }
+
+        private void sete_Click(object sender, RoutedEventArgs e)
+        {
+            display.Text += "7";
+        }
+
+        private void zero_Click(object sender, RoutedEventArgs e)
+        {
+            display.Text += "0";
+        }
+
+        private void um_Click(object sender, RoutedEventArgs e)
+        {
+            display.Text += "1";
+        }
+
+        private void dois_Click(object sender, RoutedEventArgs e)
+        {
+            display.Text += "2";
+        }
+
+        private void tres_Click(object sender, RoutedEventArgs e)
+        {
+            display.Text += "3";
+        }
+
+        private void quatro_Click(object sender, RoutedEventArgs e)
+        {
+            display.Text += "4";
+        }
+
+        private void cinco_Click(object sender, RoutedEventArgs e)
+        {
+            display.Text += "5";
+        }
+
+        private void seis_Click(object sender, RoutedEventArgs e)
+        {
+            display.Text += "6";
+        }
+
+        private void oito_Click(object sender, RoutedEventArgs e)
+        {
+            display.Text += "8";
+        }
+
+        private void nove_Click(object sender, RoutedEventArgs e)
+        {
+            display.Text += "9";
+        }
+
+        private void soma_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (operacao == "*" || operacao == "-" || operacao == "/")
+            {
+                operacao = "+";
+            }
+            else
+            {
+                acumula += double.Parse(display.Text);
+                display.Text = "";
+                operacao = "+";
+            }
+
+        }
+
+        private void igual_Click(object sender, RoutedEventArgs e)
+        {
+            if (operacao == "+")
+            {
+                acumula += double.Parse(display.Text);
+                display.Text = acumula.ToString();
+            }
+            else if (operacao == "-")
+            {
+                acumula -= double.Parse(display.Text);
+                display.Text = acumula.ToString();
+            }
+            else if (operacao == "*")
+            {
+                acumula *= double.Parse(display.Text);
+                display.Text = acumula.ToString();
+            }
+            else if (operacao == "/")
+            {
+                if (double.Parse(display.Text) != 0)
+                {
+                    acumula /= double.Parse(display.Text);
+                    display.Text = acumula.ToString();
+                }
+                else
+                {
+                    display.Text = "Dividindo por zero";
+                }
+            }
+        }
+
+        private void virgula_Click(object sender, RoutedEventArgs e)
+        {
+            display.Text += ",";
+        }
+
+        private void subtracao_Click(object sender, RoutedEventArgs e)
+        {
+            if (operacao == "*" || operacao == "+" || operacao == "/")
+            {
+                operacao = "-";
+            }
+            else
+            {
+                acumula = double.Parse(display.Text);
+                display.Text = "";
+                operacao = "-";
+            }
+        }
+
+        private void multiplicacao_Click(object sender, RoutedEventArgs e)
+        {
+            if (operacao == "-" || operacao == "+" || operacao == "/")
+            {
+                operacao = "*";
+            }
+            else
+            {
+                acumula = double.Parse(display.Text);
+                display.Text = "";
+                operacao = "*";
+            }
+        }
+
+        private void divisao_Click(object sender, RoutedEventArgs e)
+        {
+            if (operacao == "*" || operacao == "+" || operacao == "-")
+            {
+                operacao = "/";
+            }
+            else
+            {
+                acumula = double.Parse(display.Text);
+                display.Text = "";
+                operacao = "/";
+            }
+        }
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            display.Text = "";
+            operacao = "";
         }
     }
 }
