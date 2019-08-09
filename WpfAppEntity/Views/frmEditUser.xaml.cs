2ceb80bd3ed071cf8219f80e884f54e66b41f0c9
Treinamento HBSIS <treinamento@hbsis.com.br>
2019-08-09 11:54:58 -0300

    window

---
+++ WpfAppEntity/Views/frmEditUser.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfAppEntity.Views
+{
+    /// <summary>
+    /// Interaction logic for frmEditUser.xaml
+    /// </summary>
+    public partial class frmEditUser : Window
+    {
+        public frmEditUser()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            user.Nome = tbxName.Text;
+            user.Login = tbxLogin.Text;
+            user.Senha = tbmSenha.Text;
+        }
+    }
+}
